19d38909e9952f618d5f1500fbee40ce6d8c8db1
chadlanoway <112406839+chadlanoway@users.noreply.github.com>
2025-03-15 13:43:09 -0500

    lesson 3

--- js/main.js
+++ js/main.js
@@ -60,6 +60,39 @@ var arraysArray = [
     ['Green Bay', 104057]
 ];
 
+
+//above Example 2.8 line 20
+var x = d3.scaleLinear()
+    .range([90, 750]) // Instead of 810
+    .domain([0, 3]);
+
+//above Example 2.8 line 20
+    //find the minimum value of the array
+    var minPop = d3.min(cityPop, function(d){
+        return d.population;
+    });
+
+    //find the maximum value of the array
+    var maxPop = d3.max(cityPop, function(d){
+        return d.population;
+    });
+
+    //scale for circles center y coordinate
+    var y = d3.scaleLinear()
+        .range([450, 50]) //was 440, 95
+        .domain([0, 700000]); //was minPop, maxPop
+
+ //color scale generator 
+ var color = d3.scaleLinear()
+ .range([
+     "#FDBE85",
+     "#D94701"
+ ])
+ .domain([
+     minPop, 
+     maxPop
+ ]);
+
 //Example 2.6 line 3
 var circles = container.selectAll(".circles") //create an empty selection
 .data(cityPop) //here we feed in an array
@@ -74,14 +107,72 @@ var circles = container.selectAll(".circles") //create an empty selection
     var area = d.population * 0.01;
     return Math.sqrt(area/Math.PI);
 })
+//Example 2.8 line 34
 .attr("cx", function(d, i){
-    //use the index to place each circle horizontally
-    return 90 + (i * 180);
+    //use the scale generator with the index to place each circle horizontally
+    return x(i);
 })
 .attr("cy", function(d){
-    //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
-    return 450 - (d.population * 0.0005);
-});
+    return y(d.population)
+})
+.style("fill", function(d, i){ //add a fill based on the color scale generator
+    return color(d.population)
+})
+.style("stroke", "#000"); //black circle stroke
+
+ //Example 3.6 line 1...create y axis generator
+ var yAxis = d3.axisLeft(y);
+
+ var axis = container.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(50, 0)")
+        .call(yAxis);
+
+yAxis(axis);
+
+var title = container.append("text")
+.attr("class", "title")
+.attr("text-anchor", "middle")
+.attr("x", 450)
+.attr("y", 30)
+.text("City Populations");
+
+var labels = container.selectAll(".labels")
+        .data(cityPop)
+        .enter()
+        .append("text")
+        .attr("class", "labels")
+        .attr("text-anchor", "left")
+        .attr("y", function(d){
+            //vertical position centered on each circle
+            return y(d.population) ;
+        });
+
+    //first line of label
+    var nameLine = labels.append("tspan")
+        .attr("class", "nameLine")
+        .attr("x", function(d,i){
+            //horizontal position to the right of each circle
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .text(function(d){
+            return d.city;
+        });
+
+    //second line of label
+     //create format generator
+     var format = d3.format(",");
+
+     //Example 3.16 line 1...second line of label
+     var popLine = labels.append("tspan")
+         .attr("class", "popLine")
+         .attr("x", function(d,i){
+             return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+         })
+         .attr("dy", "15") //vertical offset
+         .text(function(d){
+             return "Pop. " + format(d.population); //use format generator to format numbers
+         });
 
 };
 
